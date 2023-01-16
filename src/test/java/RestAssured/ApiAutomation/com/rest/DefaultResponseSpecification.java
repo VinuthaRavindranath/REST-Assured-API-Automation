077265c8309412309255078344c1875679fe3c56
Vinutha R <vinuthar@Vinuthas-MacBook-Pro.local>
2023-01-16 20:26:07 +0530

    RequestSpecBuilderAndResponseSpecBuilder

---
+++ src/test/java/RestAssured/ApiAutomation/com/rest/DefaultResponseSpecification.java
@@ -0,0 +1,41 @@
+package RestAssured.ApiAutomation.com.rest;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class DefaultResponseSpecification {
+    String gorestUrl = "https://gorest.co.in/public/v2";
+
+
+    @BeforeTest
+    public void beforeTest() {
+        RequestSpecBuilder builder = new RequestSpecBuilder();
+        builder.setBaseUri(gorestUrl);
+        builder.addHeader("Content-Type", "application/json");
+        builder.log(LogDetail.ALL);
+        RestAssured.requestSpecification = builder.build();
+
+        ResponseSpecBuilder resBuilder = new ResponseSpecBuilder();
+        resBuilder.expectStatusCode(200);
+        resBuilder.log(LogDetail.ALL);
+        RestAssured.responseSpecification = resBuilder.build();
+    }
+
+    @Test
+    public void requestSpecExampleOne() {
+        Response response = get("/users/3633").
+                then().extract().response();
+        assertThat(response.path("id").toString(), is(equalTo(String.valueOf(3633))));
+    }
+}
---
+++ src/test/java/RestAssured/ApiAutomation/com/rest/ResponseSpecificationBuilderDemo.java
@@ -0,0 +1,42 @@
+package RestAssured.ApiAutomation.com.rest;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class ResponseSpecificationBuilderDemo {
+    RequestSpecification requestSpecification;
+    ResponseSpecification responseSpecification;
+    String gorestUrl = "https://gorest.co.in/public/v2";
+
+    @BeforeTest
+    public void beforeTest() {
+        RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
+        reqBuilder.setBaseUri(gorestUrl);
+        reqBuilder.addHeader("Content-Type", "application/json");
+        reqBuilder.log(LogDetail.ALL);
+        requestSpecification =reqBuilder.build();
+
+        ResponseSpecBuilder resBuilder = new ResponseSpecBuilder();
+        resBuilder.expectStatusCode(200);
+        resBuilder.log(LogDetail.ALL);
+        responseSpecification =resBuilder.build();
+    }
+
+    @Test
+    public void requestSpecExampleOne() {
+        Response response =given().spec(requestSpecification).get("/users/3633").
+                then().spec(responseSpecification).extract().response();
+        assertThat(response.path("id").toString(), is(equalTo(String.valueOf(3633))));
+    }
+}

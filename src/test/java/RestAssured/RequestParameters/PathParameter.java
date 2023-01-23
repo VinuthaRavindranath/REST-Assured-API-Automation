0c0d32f2e9ea3e5f99b7c4ac50fd242697831fa3
Vinutha R <vinuthar@Vinuthas-MacBook-Pro.local>
2023-01-23 21:59:59 +0530

    RequestParameters

---
+++ src/test/java/RestAssured/RequestParameters/PathParameter.java
@@ -0,0 +1,43 @@
+package RestAssured.RequestParameters;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class PathParameter {
+    String gorestUrl = "https://gorest.co.in/public/v2/";
+    String authToken = "Bearer Token";
+
+
+    @BeforeTest
+    public void beforeTest() {
+        RequestSpecBuilder builder = new RequestSpecBuilder();
+        builder.setBaseUri(gorestUrl);
+        builder.addHeader("Content-Type", "application/json");
+        builder.addHeader("Authorization", authToken);
+        builder.log(LogDetail.ALL);
+        RestAssured.requestSpecification = builder.build();
+
+        ResponseSpecBuilder resBuilder = new ResponseSpecBuilder();
+        // resBuilder.expectStatusCode(200);
+        resBuilder.log(LogDetail.ALL);
+        RestAssured.responseSpecification = resBuilder.build();
+    }
+
+    @Test
+    public void getRequestWithSingleQueryParameter() {
+        given().
+                pathParam("userId", 61023).
+                log().all().
+                when().
+                get("users/{userId}").
+                then().
+                log().all().
+                assertThat().statusCode(200);
+    }
+}

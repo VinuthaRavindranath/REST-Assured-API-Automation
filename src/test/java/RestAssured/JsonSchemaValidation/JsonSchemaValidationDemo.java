cb72b44e12c4d9e18706e9b1a4f7b6a02170dca4
Vinutha R <vinuthar@Vinuthas-MacBook-Pro.local>
2023-01-30 21:07:09 +0530

    jsonSchemaValidation

---
+++ src/test/java/RestAssured/JsonSchemaValidation/JsonSchemaValidationDemo.java
@@ -0,0 +1,42 @@
+package RestAssured.JsonSchemaValidation;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class JsonSchemaValidationDemo {
+    String petStoreUrl = "https://petstore.swagger.io/v2/";
+
+    @BeforeTest
+    public void beforeTest() {
+        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder()
+                .setBaseUri(petStoreUrl)
+                 .addHeader("Content-Type", "application/json")
+                .log(LogDetail.ALL);
+
+        RestAssured.requestSpecification = requestSpecBuilder.build();
+
+
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder()
+                .log(LogDetail.ALL);
+
+        RestAssured.responseSpecification = responseSpecBuilder.build();
+    }
+
+    @Test
+    public void validate_Post_Request_BDD_Style() {
+        File file = new File(System.getProperty("user.dir") +"/src/main/java/Resources/PetPostRequestPayload.json");
+         given().body(file).
+                when().post("pet").
+                then().assertThat().body(matchesJsonSchemaInClasspath(("PetResponseSchema.json")));
+    }
+}
